b05d582cb4c33efb21501732b981f5aebc092a69
Eric Yang <eyoung.dev@gmail.com>
2025-10-01 00:41:31 +0800

    feat: Add Profile component

--- packages/core/src/components/WalletButton/WalletButton.tsx
+++ packages/core/src/components/WalletButton/WalletButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { Profile } from './Profile'
 import './WalletButton.css'
 
 export interface WalletButtonProps {
@@ -17,7 +18,6 @@ export const WalletButton: React.FC<WalletButtonProps> = ({
   className = '',
   size = 'medium'
 }: WalletButtonProps) => {
-
 
   return (
     <div className={`wallet-button wallet-button--${size} ${className}`}>
@@ -77,16 +77,16 @@ export const WalletButton: React.FC<WalletButtonProps> = ({
                         className="wallet-button__account"
                       >
                         <span className='wallet-button__status-bot'></span>
-                        <svg xmlns="http://www.w3.org/2000/svg" 
+                        <svg xmlns="http://www.w3.org/2000/svg"
                         className='wallet-icon'
-                        width="24" 
-                        height="24" 
+                        width="24"
+                        height="24"
                         viewBox="0 0 24 24"
-                        fill="none" 
-                        stroke="currentColor" 
-                        stroke-width="2" 
-                        stroke-linecap="round" 
-                        stroke-linejoin="round" 
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
                         aria-hidden="true">
                           <path d="M19 7V4a1 1 0 0 0-1-1H5a2 2 0 0 0 0 4h15a1 1 0 0 1 1 1v4h-3a2 2 0 0 0 0 4h3a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1"></path>
                           <path d="M3 5v14a2 2 0 0 0 2 2h15a1 1 0 0 0 1-1v-4"></path>
@@ -95,6 +95,8 @@ export const WalletButton: React.FC<WalletButtonProps> = ({
                           {account.displayName}
                         </span>
                       </button>
+
+                      <Profile openAccountModal={openAccountModal} />
                     </div>
                   )
                 })()}
